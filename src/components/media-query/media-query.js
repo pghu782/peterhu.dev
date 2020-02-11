436b101310634c68674cb9de2412e4baac5e3ee7
dokku <peterx96@hotmail.com>
2020-02-10 19:54:54 -0700

    First deployment commit

--- src/components/media-query/media-query.js
+++ src/components/media-query/media-query.js
@@ -13,10 +13,6 @@ const Mobile = ({ children }) => {
   const isMobile = useMediaQuery({ maxWidth: 767 })
   return isMobile ? children : null
 }
-// const Default = ({ children }) => {
-//   const isNotMobile = useMediaQuery({ minWidth: 768 })
-//   return isNotMobile ? children : null
-// }
 
 // const Example = () => (
 //   <div>
