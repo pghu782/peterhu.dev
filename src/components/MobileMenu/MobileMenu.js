47ad943ac2cf19c945fc989f8f9ad15f7bbe041b
peterghu <peterh782@gmail.com>
2020-03-23 22:26:00 -0600

    Update mobile menu links

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 import { bool } from "prop-types"
 import {
   AiOutlineMail,
@@ -21,16 +22,20 @@ const MobileMenu = ({ open }) => {
   return (
     <div open={open} className={menuClasses}>
       <div className="row">
-        <button type="button" className="btn btn-circle">
-          <AiOutlineCode />
-        </button>
-        <a href="/projects">Projects</a>
+        <Link to="/projects/" className="m-0 p-0">
+          <button type="button" className="btn btn-circle">
+            <AiOutlineCode />
+          </button>
+        </Link>
+        <Link to="/projects/">Projects</Link>
       </div>
       <div className="row">
-        <button type="button" className="btn btn-circle">
-          <AiOutlineMail />
-        </button>
-        <a href="peterh782@gmail.com">Email</a>
+        <a href="mailto:peterh782@gmail.com" className="m-0 p-0">
+          <button type="button" className="btn btn-circle">
+            <AiOutlineMail />
+          </button>
+        </a>
+        <a href="mailto:peterh782@gmail.com">Email</a>
       </div>
       <div className="row">
         <button
@@ -38,24 +43,20 @@ const MobileMenu = ({ open }) => {
           className="btn btn-circle"
           onClick={openLink.bind(
             this,
-            "https://www.linkedin.com/in/peter-hu-p-eng-57450071/"
+            "https://www.linkedin.com/in/peter-hu-57450071/"
           )}
         >
           <AiOutlineLinkedin />
         </button>
-        <a href="https://www.linkedin.com/in/peter-hu-p-eng-57450071/">
-          LinkedIn
-        </a>
+        <a href="https://www.linkedin.com/in/peter-hu-57450071/">LinkedIn</a>
       </div>
       <div className="row">
-        <button
-          type="button"
-          className="btn btn-circle"
-          onClick={openLink.bind(this, "https://github.com/peterghu")}
-        >
-          <AiOutlineGithub />
-        </button>
-        <a href="https://github.com/peterghu">CODE</a>
+        <a href="https://github.com/peterghu" className="m-0 p-0">
+          <button type="button" className="btn btn-circle">
+            <AiOutlineGithub />
+          </button>
+        </a>
+        <a href="https://github.com/peterghu">GITHUB</a>
       </div>
     </div>
   )
